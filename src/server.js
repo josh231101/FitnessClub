40db64d46464ea904291cebf6dba8a33bc495154
josh231101 <wiijosue333@gmail.com>
2020-09-30 12:56:54 -0500

    Start creating routes and mongodb connection, create MVM

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');//Allows us to make requests from different devices
 const app = express();
+const UserController = require('./controllers/RegisterController')
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
+
+//If we start the server from production
+if(process.env.NODE_ENV != 'production'){ require('dotenv').config()}
+
+app.use(cors());
+//Returns a middleware that passes  JSON as a response
+app.use(express.json());
+
+try {
+  mongoose.connect(process.env.MONGO_DB_CONNECTION, {useNewUrlParser : true, useUnifiedTopology : true})
+} catch (e) {
+  console.log(e);
+} finally {
+  console.log("CONNECTION!");
+}
 
 app.get("/", (req,res)=>{
-  res.setEncoding("Hello from express");
+  res.send("Hola Mundo")
 })
+
+app.post('/register', RegisterController.store())
 app.listen(PORT, () =>{
   console.log(`Listenning on ${PORT}`);
 })
