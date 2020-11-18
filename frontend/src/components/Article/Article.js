d24494af48f5af9b012031eb9c862b072919de80
josh231101 <wiijosue333@gmail.com>
2020-11-17 20:47:08 -0600

    Resolve conflicts

--- frontend/src/components/Article/Article.js
+++ frontend/src/components/Article/Article.js
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Article.css";
 
-<<<<<<< HEAD
 const Article = ({ bgImg, title, author }) => {
-=======
-const Article = ({ bgImg, title }) => {
->>>>>>> c2e23ab1e1d00ca8919335ed85318a0d5dde2419
   return (
     <article className="article">
       <div
@@ -13,10 +9,7 @@ const Article = ({ bgImg, title }) => {
         className="service__bg"
       ></div>
       <h4 className="article__title">{title}</h4>
-<<<<<<< HEAD
       <p className="article__author">- {author}</p>
-=======
->>>>>>> c2e23ab1e1d00ca8919335ed85318a0d5dde2419
     </article>
   );
 };
