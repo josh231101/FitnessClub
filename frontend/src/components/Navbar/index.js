e166e469324cae0efe9f12d6b8d6952a5d993455
josh231101 <wiijosue333@gmail.com>
2020-11-26 19:30:43 -0600

    Add navbar scrolling

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link as LinkS } from "react-scroll";
 import { Link as LinkR } from "react-router-dom";
 import "./NavbarElements.css";
 
 const Navbar = () => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      document
+        .getElementsByClassName("navbar")[0]
+        .classList.remove("navbar--transparent");
+      document
+        .getElementsByClassName("navbar")[0]
+        .classList.add("navbar--active");
+    } else {
+      document
+        .getElementsByClassName("navbar")[0]
+        .classList.remove("navbar--active");
+      document
+        .getElementsByClassName("navbar")[0]
+        .classList.add("navbar--transparent");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
   return (
-    <nav class="navbar">
-      <LinkR class="navbar__title" to="/">
-        Fit<span>Club</span>
-      </LinkR>
-      <div class="navbar__links">
-        <LinkS to="about">About Us</LinkS>
-        <LinkS to="features">Features</LinkS>
-        <LinkS to="features">Events</LinkS>
-        <LinkR to="/login">Log In</LinkR>
-        <LinkR to="/register">Register</LinkR>
+    <nav class="navbar navbar--transparent">
+      <div class="navbar__wrapper">
+        <LinkR class="navbar__title" to="/">
+          Fit<span>Club</span>
+        </LinkR>
+        <div class="navbar__links">
+          <LinkS to="about">About Us</LinkS>
+          <LinkS to="features">Features</LinkS>
+          <LinkS to="features">Events</LinkS>
+          <LinkR to="/login">Log In</LinkR>
+          <LinkR to="/register">Register</LinkR>
+        </div>
       </div>
     </nav>
   );
