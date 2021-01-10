41eeabcc375d3db1ef54f06f08efd555266d7351
Josue Arreola <wiijosue333@gmail.com>
2021-01-10 16:44:28 -0600

    Update baseURL API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Initialize our API to request the info
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://api-fitclub.herokuapp.com'
 })
 
 export default api;
