038935e9a518e58cc4dee4b2d991c319c3a6029a
Josue Arreola <wiijosue333@gmail.com>
2021-01-15 20:51:52 -0600

    Add new jest tests

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');//Allows us to make requests from different devices
 const routes = require('./routes')
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 //If we start the server from production
 //if(process.env.NODE_ENV != 'production'){ require('dotenv').config()}
